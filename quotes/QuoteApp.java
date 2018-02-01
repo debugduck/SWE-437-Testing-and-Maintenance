cf56c9fb68639a01af836770f26d674350d3ef3a
david guo <davidbguo@gmail.com>
2018-02-01 17:02:07 -0500

    basic shell of commandline program

---
+++ quotes/QuoteApp.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+import static java.lang.System.out;
+
+class QuoteApp {
+    
+    private static Scanner scanner = new Scanner( System.in );
+    
+    public static void main(String[] args) {
+        commandsMenu();
+    	String command = "none";
+        boolean done = false;
+        while (!done) {
+        	command = scanner.nextLine();
+        	if (command.equalsIgnoreCase("c")) {
+        		commandsMenu();
+        	} else if (command.equalsIgnoreCase("rq")) {
+        		randomQuote();
+        	} else if (command.equalsIgnoreCase("rs")) {
+        		recentSearches();
+        	} else if (command.equalsIgnoreCase("s")) {
+        		searchQuotes();
+        	} else if (command.equalsIgnoreCase("q")) {
+        		done = true;
+        	} else {
+        		out.println("Invalid command, try again.");
+        	}
+        }
+    }
+    
+    private static void commandsMenu() {
+    	out.println("~~~~~~~~~~~~~~~~~COMMANDS~~~~~~~~~~~~~~~~~");
+    	out.println("c = Display Commands List");
+    	out.println("rq = Get Random Quote");
+    	out.println("rs = Display Recent Searches");
+    	out.println("s = Search Quotes");
+    	out.println("q = Quit Program");
+    	out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+    
+    private static void randomQuote() {
+    	out.println("~~~~~~~~~~~~~~~~~RANDOM QUOTE~~~~~~~~~~~~~~~~~");
+    	out.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. - Lorem Ipsum");
+    	out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+    
+    private static void recentSearches() {
+    	out.println("~~~~~~~~~~~~~~~~~RECENT SEARCHES~~~~~~~~~~~~~~~~~");
+    	out.print("Do you want to display recent user searches or community searches? (u or c): ");
+    	String input = "none";
+    	boolean valid = false;
+    	while( !valid ) {
+        	input = scanner.nextLine();
+        	if (input.equalsIgnoreCase("u")) {
+        		valid = true;
+        		recentUserSearch();
+        	} else if (input.equalsIgnoreCase("c")) {
+        		valid = true;
+        		recentCommSearch();
+        	} else {
+        		out.println("Invalid command, try again.");
+        	}
+    	}
+    	out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+    
+    private static void recentUserSearch() {
+    	out.println("1. This");
+    	out.println("2. is");
+    	out.println("3. just");
+    	out.println("4. a");
+    	out.println("5. Mockup");
+    }
+    
+    private static void recentCommSearch() {
+    	out.println("1. This");
+    	out.println("2. is");
+    	out.println("3. just");
+    	out.println("4. a");
+    	out.println("5. Mockup");
+    }
+    
+    private static void searchQuotes() {
+    	out.println("~~~~~~~~~~~~~~~~~SEARCH QUOTES~~~~~~~~~~~~~~~~~");
+    	out.println("Select a scope- quote (q) author (a) both (b): ");
+    	String inputScope = "none";
+    	String inputString = "none";
+    	boolean valid = false;
+    	while( !valid ) {
+    		inputScope = scanner.nextLine();
+        	if (inputScope.equalsIgnoreCase("q")) {
+        		valid = true;
+        	} else if (inputScope.equalsIgnoreCase("a")) {
+        		valid = true;
+        	}  else if (inputScope.equalsIgnoreCase("b")) {
+        		valid = true;
+        	} else {
+        		out.println("Invalid command, try again.");
+        	}
+    	}
+    	out.println("Type in search string: ");
+    	inputString = scanner.nextLine();
+    	out.println(">>>Now we do some search in scope " + inputScope + " with string: " + inputString);
+    	out.println("Results: ");
+    	out.println("1. This");
+    	out.println("2. is");
+    	out.println("3. just");
+    	out.println("4. a");
+    	out.println("5. Mockup");
+    	
+    	out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+    
+}
