92af1bf0a30a430f473bbe4addc6f93a91f6ab16
david guo <davidbguo@gmail.com>
2018-03-01 17:12:05 -0500

    basic changes so Quote now contains an ArrayList for keywords

--- src/quotes/Quote.java
+++ src/quotes/Quote.java
@@ -1,5 +1,6 @@
 package quotes;
 
+import java.util.*;
 /**
  * Quote data object.
  * @author Mongkoldech Rajapakdee & Jeff offutt
@@ -9,44 +10,83 @@ package quotes;
  */
 public class Quote
 {
-   private String author;
-   private String quoteText;
+	private String author;
+	private String quoteText;
+	private ArrayList<String> keyWords;
 
-   // Default constructor does nothing
-   public Quote ()
-   {
-   }
+	// Default constructor does nothing
+	public Quote ()
+	{
+	}
 
-   // Constructor that assigns both strings
-   public Quote (String author, String quoteText)
-   {
-      this.author = author;
-      this.quoteText = quoteText;
-   }
+	// Constructor that assigns both strings
+	public Quote (String author, String quoteText, ArrayList<String> keywords)
+	{
+		this.author = author;
+		this.quoteText = quoteText;
+		if (checkLimit(keywords))
+		{
+			keyWords = keywords;
+			Collections.sort(keyWords);
+		}
+		else
+			keyWords = new ArrayList<String>();
+		
+	}
 
-   // Getter and setter for author
-   public String getAuthor ()
-   {
-      return author;
-   }
-   public void setAuthor (String author)
-   {
-      this.author = author;
-   }
+	// Getter and setter for author
+	public String getAuthor ()
+	{
+		return author;
+	}
+	public void setAuthor (String author)
+	{
+		this.author = author;
+	}
 
-   // Getter and setter for quoteText
-   public String getQuoteText ()
-   {
-      return quoteText;
-   }
-   public void setQuoteText (String quoteText)
-   {
-      this.quoteText = quoteText;
-   }
+	// Getter and setter for quoteText
+	public String getQuoteText ()
+	{
+		return quoteText;
+	}
+	public void setQuoteText (String quoteText)
+	{
+		this.quoteText = quoteText;
+	}
 
-   @Override
-   public String toString ()
-   {
-      return "Quote {" + "author='" + author + '\'' + ", quoteText='" + quoteText + '\'' + '}';
-   }
+	// Getter and setter for keyWords
+	public ArrayList<String> getKeyWords ()
+	{
+		return keyWords;
+	}
+	public boolean setKeyWords (ArrayList<String> keyWords)
+	{
+		if (checkLimit(keyWords))
+		{
+			this.keyWords = keyWords;
+			Collections.sort(keyWords);
+			return true;
+		} else 
+		{
+			return false;
+		}
+
+	}
+
+
+	@Override
+	public String toString ()
+	{
+		return "Quote {" + "author='" + author + '\'' + ", quoteText='" + quoteText + '\'' + ", keyWords='" + keyWords.toString() + '}';
+	}
+
+	private boolean checkLimit(ArrayList<String> keyWords)
+	{
+		if (keyWords.size() > 5)
+			return false;
+		for (String s : keyWords)
+			if (s.length() > 44)
+				return false;
+		return true;
+	}
 }
