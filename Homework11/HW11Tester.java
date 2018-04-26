3da07a10c29fd2a4f7c299b4c0de69070403a9f3
david guo <davidbguo@gmail.com>
2018-04-26 17:32:50 -0400

    finished tests for hw11

--- Homework11/HW11Tester.java
+++ Homework11/HW11Tester.java
@@ -3,6 +3,9 @@ import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+
+import hw5.Cal;
+
 import static org.junit.Assert.*;
 import java.util.*;
 
@@ -12,21 +15,39 @@ import java.util.*;
 public class HW11Tester {
 	
 	@Test 
-	public void P1R1() {}
+	public void P1R1() {
+		int retVal = Cal.cal(5, 20, 5, 20, 2013);
+		assertTrue("P1R1 failed " + retVal, retVal == 0);
+	}
 	
 	@Test 
-	public void P1R2() {}
+	public void P1R2() {
+		int retVal = Cal.cal(5, 20, 6, 20, 2013);
+		assertTrue("P1R2 failed " + retVal, retVal == 31);
+	}
 	
 	@Test 
-	public void P2R3() {}
+	public void P2R3() {
+		int retVal = Cal.cal(2, 20, 3, 20, 2013);
+		assertTrue("P2R3 failed " + retVal, retVal == 28);
+	}
 	
 	@Test 
-	public void P2R5() {}
+	public void P2R5() {
+		int retVal = Cal.cal(2, 20, 3, 20, 100);
+		assertTrue("P2R5 failed " + retVal, retVal == 28);
+	}
 	
 	@Test 
-	public void P2R6() {}
+	public void P2R6() {
+		int retVal = Cal.cal(2, 20, 3, 20, 400);
+		assertTrue("P2R6 failed " + retVal, retVal == 29);
+	}
 	
 	@Test 
-	public void P2R7() {}
+	public void P2R7() {
+		int retVal = Cal.cal(2, 20, 3, 20, 8);
+		assertTrue("P2R7 failed " + retVal, retVal == 29);
+	}
 	
 }
